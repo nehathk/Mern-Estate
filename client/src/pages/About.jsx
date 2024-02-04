9b70472478ea67a1be291c2f5405932798988010
Neha <nehathk03@gmail.com>
2024-02-04 10:28:40 +0530

    created basic routes and server

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi vel amet laboriosam, excepturi nostrum cum magni ratione fuga necessitatibus aliquam, debitis tempora iste ut eligendi facere ad similique consequuntur perspiciatis?
+    </div>
+  )
+}
