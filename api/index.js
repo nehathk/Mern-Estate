95591c6837f518c7ec8317c2d1be320741776eaf
Neha <nehathk03@gmail.com>
2024-02-06 13:08:08 +0530

    created a middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -23,3 +23,14 @@ app.get('/',(req,res)=>{
 })
 app.use('/api/user' ,userRouter);
 app.use('/api/auth',authRouter);
+app.use((err,req,res,next)=>{
+    const statusCode= err.statusCode || 500;
+    const message= err.message ||`Internal server error`;
+    return res.status(statusCode).json({
+        succcess:false,
+        statusCode,
+        message
+
+
+    });
+});
